cd8f2e1e68f594471f359f5cbf13e6fe45e0acc1
Jonas Hansen <jonas.hansen@bluewin.ch>
2016-05-23 19:31:12 +0200

    lzw encoding 

--- test/compression_lzw_test.py
+++ test/compression_lzw_test.py
@@ -1,9 +1,10 @@
 import lzw
 from unittest import TestCase
 
+
 class TestEncodeLZW(TestCase):
     def test_encode(self):
-        lzw.encode([1,4,3,2,6],lzw.encode("aaacbcbcbc"))
+        self.assertEqual([1, 4, 3, 2, 6, 8], lzw.encode("aaacbcbcbc"))
 
     def test_decode(self):
-        lzw.decode("aaacbcbcbc", lzw.decode([1,4,3,2,6]))
+        lzw.decode("aaacbcbcbc", lzw.decode([1, 4, 3, 2, 6]))
