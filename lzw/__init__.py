d87bc6bbf1b47b071ce765a25194077e72b7e253
Michi Zurmühle <mzurmuehle@gmail.com>
2016-05-23 19:46:20 +0200

    decompression lzw

--- lzw/__init__.py
+++ lzw/__init__.py
@@ -1,6 +1,29 @@
-def encode(input_data, dictionary=["a","b","c"]):
+import copy
+
+
+def encode(input_data, dictionary=["a", "b", "c"]):
     pass
+
+
+basic_dictionary = ["a", "b", "c"]
 
 
 def decode(input_data):
-    pass
+    dictionary = copy.copy(basic_dictionary)
+    dictionary.insert(0, None)
+
+    result_entries = []
+    for next_index in input_data:
+        if next_index >= len(dictionary):
+            new_entry = ''.join([result_entries[-1], (result_entries[-1])[0]])
+            dictionary.append(new_entry)
+            result_entries.append(new_entry)
+        else:
+            result_entries.append(dictionary[next_index])
+            if len(result_entries) > 1:
+                dictionary.append(''.join([result_entries[-2], (result_entries[-1])[0]]))
+
+    print("dictionary: {}".format(dictionary))
+
+    return ''.join(result_entries)
+
--- test/compression_lzw_test.py
+++ test/compression_lzw_test.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 class TestEncodeLZW(TestCase):
     def test_encode(self):
-        lzw.encode([1,4,3,2,6],lzw.encode("aaacbcbcbc"))
+        self.assertEqual([1,4,3,2,6],lzw.encode("aaacbcbcbc"))
 
     def test_decode(self):
-        lzw.decode("aaacbcbcbc", lzw.decode([1,4,3,2,6]))
+        self.assertEqual("aaacbcbcbc", lzw.decode([1,4,3,2,6,8]))
