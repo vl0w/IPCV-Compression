055dd6c6ff4afeef0174b6c7c37bcf2b74ecd7f0
Jonas Hansen <jonas.hansen@bluewin.ch>
2016-05-24 21:22:05 +0200

    Missing test for LZW

--- test/compression_lzw_test.py
+++ test/compression_lzw_test.py
@@ -6,16 +6,18 @@ class TestEncodeLZW(TestCase):
     def test_encode(self):
         self.assertEqual([1, 4, 3, 2, 6, 8], lzw.encode("aaacbcbcbc"))
 
+
+    def test_encode_with_non_default_dictionary(self):
+        base_dictionary = ["t", "i", "y", "n", "b", " "]
+        self.assertEqual([2, 1, 1, 3, 6, 5, 7, 9, 11, 7, 17], lzw.encode("itty bitty bit bin", base_dictionary))
+
     def test_decode(self):
         self.assertEqual("aaacbcbcbc", lzw.decode([1, 4, 3, 2, 6, 8]))
 
     def test_encode_decode(self):
         base_dictionary = ["t", "i", "y", "n", "b", " "]
-        expected_encoded = [2, 1, 1, 3, 6, 5, 7, 9, 11, 7]
-        expected_decoded = "itty bitty bit"
+        input = "itty bitty bit bin"
 
-        actual_encoded = lzw.encode(expected_decoded, base_dictionary)
-        self.assertEqual(expected_encoded, actual_encoded)
-
-        actual_decoded = lzw.decode(actual_encoded, base_dictionary)
-        self.assertEqual(expected_decoded, actual_decoded)
+        encoded = lzw.encode("itty bitty bit bin", base_dictionary)
+        decoded = lzw.decode(encoded, base_dictionary)
+        self.assertEqual(input, decoded)
